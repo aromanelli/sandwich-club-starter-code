9c93fc08656713c6848340ff8615c2d4bce78563
aromanelli <adrian.romanelli@gmail.com>
2018-04-25 22:25:55 -0700

    Implemented view logic.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
+import java.util.ListIterator;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,11 +24,10 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
-
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
+            return; // Reviewer: I added this here, seemed like it was necessary to prevent NPE
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
@@ -43,12 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
+        populateUI(sandwich);
     }
 
     private void closeOnError() {
@@ -56,7 +54,42 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(final Sandwich sandwich) {
 
+        setTitle(sandwich.getMainName());
+
+        final ImageView ingredientsIv = findViewById(R.id.image_iv);
+        Picasso.with(this)
+                .load(sandwich.getImage())
+                .into(ingredientsIv);
+
+        final TextView alsoKnownAs = findViewById(R.id.also_known_tv);
+        alsoKnownAs.setText(listToString(sandwich.getAlsoKnownAs()));
+
+        final TextView placeOfOrigin = findViewById(R.id.origin_tv);
+        placeOfOrigin.setText(sandwich.getPlaceOfOrigin());
+
+        final TextView description = findViewById(R.id.description_tv);
+        description.setText(sandwich.getDescription());
+
+        final TextView ingredients = findViewById(R.id.ingredients_tv);
+        ingredients.setText(listToString(sandwich.getIngredients()));
+
+    }
+
+    private String listToString(final List<String> list) {
+        final StringBuilder builder = new StringBuilder();
+        // Reviewer: Didn't know if I could change minSdkVersion to use Java 8 features
+        //           or not, so stayed at 16, which means I cannot use .forEach(), etc.
+        if (list != null) {
+            ListIterator<String> listIterator = list.listIterator();
+            while (listIterator.hasNext()) {
+                builder.append(listIterator.next());
+                if (listIterator.hasNext()) {
+                    builder.append(", ");
+                }
+            }
+        }
+        return builder.toString();
     }
 }
